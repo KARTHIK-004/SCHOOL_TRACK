550d879cccd755dc803e996487480f0d02d7f550
KARTHIK-004 <selvamk911@gmail.com>
2025-04-06 18:18:23 +0530

    FEAT : initialize project with React and TailwindCSS

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default Home;
