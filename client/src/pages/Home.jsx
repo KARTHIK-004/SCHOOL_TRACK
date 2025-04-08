0ce1bcc03691b4a9455f187138ad28659d7a0c71
KARTHIK-004 <selvamk911@gmail.com>
2025-04-08 15:04:02 +0530

    FEAT(theme): add dark mode toggle and theme context

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
+      <div className="flex items-center gap-4">
+        <Button>Click me</Button>
+        <ModeToggle />
+      </div>
     </div>
   );
 };
